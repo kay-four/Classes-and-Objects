98d71cd711e805488a454b90303b42bb98ba5799
kay-four <88488188+kay-four@users.noreply.github.com>
2022-05-27 19:43:14 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,35 @@
+class Student:
+    # [assignment] Skeleton class. Add your code here
+    def __init__(self,name,age,tracks,score):
+        self.name = str(name)
+        self.age = int(age)
+        self.tracks = list(tracks)
+        self.score = float(score)
+        
+
+    def change_name(self,name):
+        self.name = name
+        
+        
+
+    def change_age(self,age):
+        self.age = age
+        
+
+    def add_track(self,tracks):
+        self.tracks = tracks
+        
+
+    def get_score(self):
+        return self.score
+
+
+
+Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
+
+
+# Expected methods
+Bob.change_name("Peter")
+Bob.change_age(34)
+Bob.add_track("UI/UX")
+Bob.get_score()
